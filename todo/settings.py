7e1aee4499b18519763fcc05068758323547e523
Walid El Amrouchi <oualid.elamrouchi@usmba.ac.ma>
2026-03-13 02:45:42 +0000

    auto migrate

--- todo/settings.py
+++ todo/settings.py
@@ -84,7 +84,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
+
+
 }
 
 
